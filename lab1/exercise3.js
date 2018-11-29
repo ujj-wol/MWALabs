2dcd66a67f18f47ea16df8461833b2f076a072fd
Ujjwol Bhandari <ubhandari2@pc-uzol.local>
2018-11-28 19:50:15 -0600

    add function currying as in lecture

--- lab1/exercise3.js
+++ lab1/exercise3.js
@@ -3,18 +3,28 @@ const item = {
     'type': 'regular',
     'category': 'food',
     'price': 200
-}
+};
 
 function applyCoupon(item) {
-    //console.log(item.price);
+    console.log(item.price);
     return function(discount) {
         item.price = item.price-(discount*item.price/100);
-        //console.log(item.price);
+        console.log(item.price);
         return item;
     }
 }
-console.log(applyCoupon(item)(10).price === 180);
+// console.log(applyCoupon(item)(10).price === 180);
 
+function applyCoupon1(item, discount) {
+    console.log(item.price);
+    item.price = item.price-(discount*item.price/100);
+    console.log(item.price);
+    return item;
+}
+//console.log(applyCoupon1(item, 10).price === 180);
+
+var applyCouponNew = applyCoupon1.bind({},item);
+console.log(applyCouponNew(10).price == 180);
 
 // function diff(x) {
 //     return function(y) {
