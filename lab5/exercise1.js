ab53539d40bc0b2d449523c5c55f5545e8c87992
Ujjwol Bhandari <ubhandari2@pc-uzol.local>
2018-12-02 19:05:15 -0600

    update lab5exercise

--- lab5/exercise1.js
+++ lab5/exercise1.js
@@ -13,17 +13,54 @@ Set standard pagination options
 Allow response caching for one day.
 */
 
+
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
+
+// allow app to run behind any proxy
+app.enable('trust proxy');
+// not to reveal framework name to client
+app.set('X-powered-by', false);
+// route should be strict
+app.set('strict routing', true);
+// route should be case-sensitive 
+app.enable('case sensitive routing');
+
+//send standard pagination options
+
+
 const port = 8080;
+const url = 'https://randomuser.me/api/?results=1';
 
-const json_obj = {name: 'Ujjwol', age: '25'};
 
-app.get('/', function(req, res) {
-    res.json(json_obj);
-    res.end();
+// app.get('/users', function(req, res) {
+//     // disallow proxy to cache response and allow response caching for 1 day to client
+//     res.set({'Cache-Control': 'private, max-age=86400',
+//             'X-powered-by': false
+//         });
+//     res.json(json_obj);
+//     axios.get(url)
+//     .then(function(response) {
+//         response.data.pipe(res);
+//         console.log(response);
+//     })
+//     .catch((err) => console.log('eroor occurred!!'));
+// });
+
+app.get('/users', (req, res) => {
+    axios.get(url)
+        .then((data) => {
+            console.log(data);
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.set('Cache-Control', 'private,max-age=86400')
+            res.end(JSON.stringify(data.data));
+        })
+        .catch((error) => {
+            res.end(`Error occurred!! \n${error.message}`)
+        });
 });
 
 app.listen(port, () => console.log(`listening to port ${port}...`));
-
