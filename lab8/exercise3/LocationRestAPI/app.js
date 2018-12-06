bee10ca6397e1cd71ac9238de335bf0a347a50df
Ujjwol Bhandari <ubhandari2@pc-uzol.local>
2018-12-05 23:47:56 -0600

    complete lab8 exercise3

--- lab8/exercise3/LocationRestAPI/app.js
+++ lab8/exercise3/LocationRestAPI/app.js
@@ -22,17 +22,33 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// setup routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/location', locationRouter);
 
-// open our database connection and insert location points to our collection
+// open our database connection
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function (err, database) {
   if (err) throw err;
   app.locals.db = database.db('MongoLabs');
+  database.db('MongoLabs').collection('lab8_ex3').createIndex({location:'2d'});
+
+  const seedLocations = [
+    {"name" : "Pizza Hut", "category" : "Restaurant", "location" : [ -91.98914, 41.00686 ] },
+    {"name" : "Walmart Supercenter", "category" : "Supercenter", "location" : [ -91.99326780000001, 41.00768800000001 ] },
+    {"name" : "Hy-Vee", "category" : "Supermarket", "location" : [ -91.9785195, 41.00467999999999 ] },
+    {"name" : "Subway ", "category" : "Restaurants", "location" : [ -91.97267169999998, 41.0067101 ] },
+    {"name" : "Fairfield Recreation Center", "category" : "Recreation Center", "location" : [ -91.97523239999998, 41.0054453 ] },
+    {"name" : "The Depot Brewery", "category" : "Night Club", "location" : [ -91.9680032, 41.01098500000001 ] }
+  ];
+
+  // database.db('MongoLabs').collection('lab8_ex3').insert(seedLocations, (err, results) => {
+  //   if(err) console.log("error during seed data insertion.");
+  //   else console.log('seed data inserted successfully to the collection');
+  // });
 
   const port = 4000;
   app.listen(port,()=> console.log(`Listening at port ${port}`));
--- lab8/exercise3/LocationRestAPI/routes/location.js
+++ lab8/exercise3/LocationRestAPI/routes/location.js
@@ -1,37 +1,70 @@
 const router = require('express').Router();
-const {check, validationResult} = require('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
 
 //default get for /api/location
 router.get('/', (req, res) => {
-    //res.end('You submit location details from here!');
-    res.json({message: 'You submit location details from here!'});
+  //res.end('You submit location details from here!');
+  res.json({
+    message: 'You submit location details from here!'
+  });
 });
 
-router.post("/", [
-    check("name", "name field is required").exists(),
-    check("category", "category needs to be defined").exists(),
-    check("location", "location needs to be defined").exists()
-  ], (req, res) => {
-    const errors = validationResult(req);
-  
-    if(!errors.isEmpty()){
-      return res.status(422).json({errors: errors.array()});
-    }
-  
-    let newDoc = {
-      name: req.body.name,
-      category: req.body.category,
-      location: req.body.location
-    };
-  
-    const collection = req.app.locals.db.collection('lab8_ex3');
-    collection.insertOne(newDoc, (err, insertedDoc) => {
-      if (err) throw err;
-      console.log('Following 1 document inserted:');
-      return res.status(200).json(insertedDoc);
-      // db.close();
+// to find nearest 3 locations (i.e documents) to our current location from our collection
+router.get("/find", [
+  check("category", "category is mandatory").exists(),
+  check("location", "location is required to find 3 points nearest to it")
+], (req, res) => {
+
+  console.log('find route entered!!');
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array()});
+  }
+
+  let searchDoc = {
+    name: req.query.name,
+    category: req.query.category,
+    location: req.query.location
+  };
+
+  const nearestPoints = req.app.locals.db.collection('lab8_ex3')
+                            .find({location: {$near: [-91.9665342, 41.017654]}}).project({_id:0}).limit(3).toArray((err, results) => {
+                              if(err) return res.status(500).json({error: err});
+
+                              res.json(results);
+                            });
+
+});
+
+// to add a document to our collection
+router.post("/add", [
+  check("name", "name field is required").exists(),
+  check("category", "category needs to be defined").exists(),
+  check("location", "location needs to be defined").exists()
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errors: errors.array()
     });
-  
+  }
+
+  let newDoc = {
+    name: req.body.name,
+    category: req.body.category,
+    location: req.body.location
+  };
+
+  const collection = req.app.locals.db.collection('lab8_ex3');
+  collection.insertOne(newDoc, (err, insertedDoc) => {
+    if (err) throw err;
+    console.log('Following 1 document inserted:');
+    return res.status(200).json(insertedDoc);
+    // db.close();
   });
 
-  module.exports = router;
+});
+
+module.exports = router;
